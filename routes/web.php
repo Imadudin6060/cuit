0f36dc0a245ff1536157209c05d8d1f337eff2cb
Imadudin6060 <chenelstory96@gmail.com>
2025-06-22 12:40:13 +0700

    pengenalan laravel dan autentikasi sederhana

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CuitController;
+use App\Http\Controllers\AuthController;
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return view('login');
+    })->name('login');
+
+    Route::get('/register', function () {
+        return view('register');
+    })->name('register');
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::get('/', [CuitController::class, 'index'])->name('home');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/post', [CuitController::class, 'post'])->name('cuit_post');
+
+Route::middleware('auth')->group(function () {
+
+});
